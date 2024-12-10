fe2e67edf7792f10384b9dc98a1b9e8bad50d5c6
Brian Sniffen <bts@evenmere.org>
2024-12-09 23:34:26 -0500

    finish day 7

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -3,14 +3,88 @@ use code_timing_macros::time_function;
 use rayon::prelude::*;
 use std::error::Error;
 
-#[time_function]
-fn part1(data: &str) -> u32 {
-    0
+fn parse_exprs(data: &str) -> Vec<(u64, Vec<u64>)> {
+    data.lines()
+        .map(|line| {
+            let (goal, rest) = line.split_once(':').expect("expected ':'");
+            let nums = rest
+                .split_whitespace()
+                .map(|n| n.parse().expect("integer elements"))
+                .collect();
+            (goal.parse().expect("integer goal"), nums)
+        })
+        .collect()
+}
+
+fn reach(goal: &u64, nums: &[u64]) -> bool {
+    let goal = *goal;
+    let nums = &nums[0..nums.len()];
+    if nums.len() == 1 {
+        return nums[0] == goal;
+    } else {
+        let last = nums.last().unwrap();
+        let a = if goal % nums.last().unwrap() == 0 {
+            reach(&(goal / last), &nums[0..nums.len() - 1])
+        } else {
+            false
+        };
+        let b = if goal >= *last {
+            reach(&(goal - last), &nums[0..nums.len() - 1])
+        } else {
+            false
+        };
+        return a || b;
+    }
+}
+
+fn reach_concat(goal: u64, nums: &[u64]) -> bool {
+    if nums.len() == 1 {
+        return nums[0] == goal;
+    } else {
+        let last = nums.last().unwrap();
+
+        let c = match goal.to_string().strip_suffix(&last.to_string()) {
+            Some(shorter) if shorter.len() > 0 => {
+                reach_concat(shorter.parse().unwrap(), &nums[0..nums.len() - 1])
+            }
+            None | Some(_) => false,
+        };
+        let a = if goal % nums.last().unwrap() == 0 {
+            reach_concat(goal / last, &nums[0..nums.len() - 1])
+        } else {
+            false
+        };
+        let b = if goal >= *last {
+            reach_concat(goal - last, &nums[0..nums.len() - 1])
+        } else {
+            false
+        };
+        return c || a || b;
+    }
 }
 
 #[time_function]
-fn part2(data: &str) -> u32 {
-    0
+fn part1(data: &str) -> u64 {
+    let exprs = parse_exprs(&data);
+    exprs
+        .iter()
+        .filter_map(|(goal, nums)| if reach(goal, nums) { Some(goal) } else { None })
+        .sum()
+}
+
+#[time_function]
+fn part2(data: &str) -> u64 {
+    let exprs = parse_exprs(&data);
+    exprs
+        .par_iter()
+        .filter_map(|(goal, nums)| {
+            if reach_concat(*goal, nums) {
+                Some(goal)
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -36,11 +110,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(&TESTDATA), 0);
+        assert_eq!(part1(&TESTDATA), 3749);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(&TESTDATA), 0);
+        assert_eq!(part2(&TESTDATA), 11387);
     }
 }
