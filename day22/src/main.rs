aa392fab7ffe1552f7b10cb2c52986d8b769224b
Brian Sniffen <bts@evenmere.org>
2024-12-22 09:09:14 -0500

    30-minute loop for day 22

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,6 +1,5 @@
 use aochelpers::get_daily_input;
 use code_timing_macros::time_function;
-use memoize::memoize;
 use rayon::prelude::*;
 use std::collections::VecDeque;
 use std::error::Error;
@@ -20,8 +19,7 @@ fn many(i: i64, n: i64) -> i64 {
     ans
 }
 
-#[memoize]
-fn sale_price(i: i64, v: Vec<i64>) -> i64 {
+fn sale_price(i: i64, a: i64, b: i64, c: i64, d: i64) -> i64 {
     let mut history = VecDeque::new();
     let mut secret = i;
     for _j in 0..4 {
@@ -39,7 +37,7 @@ fn sale_price(i: i64, v: Vec<i64>) -> i64 {
             history[4] - history[3],
         ];
         history.pop_front();
-        if v == diffs {
+        if vec![a, b, c, d] == diffs {
             return secret % 10;
         }
     }
@@ -55,20 +53,45 @@ fn part1(data: &str) -> i64 {
     ans
 }
 
+// bounded to -3..=3, we get 1504 in 185s.
+// memoize takes 332s to get the same answer!
+
 #[time_function]
 fn part2(data: &str) -> i64 {
     // sketch: for each monkey, find the sequences that get you 9, 8, 7, etc.
     // calculate a total value for each such sequence, which is about 20k such values.  But values are uniformly distributed, so there should be 200 sequences for each value, so 4m entries.  That's probably too many?
     // but just looking within -3..3 means 7^4 is only 2400.
-    // alternate sketch: genetic algorithm working from 0,0,0,0
-    let mut price = 0;
-    for line in data.lines() {
-        let i = line.parse::<i64>().expect("parse input");
-        let p = sale_price(i, vec![-2, 1, -1, 3]);
-        println!("{}: {}", i, p);
-        price += p;
-    }
-    price
+    // alternate: pre-calculate a map for each input of signal->value:
+    // HashMap from the 4 differences before the number to the number itself, then merge all the maps together and find the max value. Ran in around 450ms on my M2 macbook air, down to around 150ms after parallelizing with rayon.
+    let data: Vec<i64> = data
+        .lines()
+        .map(|line| line.parse::<i64>().expect("parse input"))
+        .collect();
+    //    let mut best_signal = vec![0, 0, 0, 0];
+    //    for a in -3..=3 {
+    (-9..9)
+        .into_par_iter()
+        .map(|a| {
+            let mut best_price = 0;
+            for b in -9..=9 {
+                for c in -9..=9 {
+                    for d in -9..=9 {
+                        //                    let signal = vec![a, b, c, d];
+                        let mut price = 0;
+                        for i in data.iter() {
+                            let p = sale_price(*i, a, b, c, d);
+                            price += p;
+                        }
+                        if price > best_price {
+                            best_price = price;
+                        }
+                    }
+                }
+            }
+            best_price
+        })
+        .max()
+        .expect("no max found")
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
